e1d8eaebc393ff33bd0eb5cf354fbeb2e62262bd
Ryan Tapfield <108175935+RyanDusty@users.noreply.github.com>
2025-06-03 09:13:41 +1000

    Update RunnableClass.java

--- src/main/java/me/runnable/RunnableClass.java
+++ src/main/java/me/runnable/RunnableClass.java
@@ -4,9 +4,11 @@ import me.CustomHealthDisplay;
 
 public class RunnableClass implements Runnable {
     private CustomHealthDisplay plugin;
+    private Set<player> players;
 
-    public RunnableClass(CustomHealthDisplay plugin) {
+    public RunnableClass(CustomHealthDisplay plugin, Set<player> players) {
         this.plugin = plugin;
+        this.players = players;
     }
     @Override
     public void run() {
