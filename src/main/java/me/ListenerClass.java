eac24e2ac4fb22a0754895eca617101867c2bae8
Ryan Tapfield <108175935+RyanDusty@users.noreply.github.com>
2025-06-03 12:35:03 +1000

    Update ListenerClass.java

--- src/main/java/me/ListenerClass.java
+++ src/main/java/me/ListenerClass.java
@@ -23,7 +23,7 @@ public class ListenerClass implements Listener {
 
     public ListenerClass(CustomHealthDisplay plugin) {
         this.plugin = plugin;
-       //RunnableClass runnableClass = new RunnableClass(plugin);
+        //RunnableClass runnableClass = new RunnableClass(plugin);
     }
 
 
@@ -38,8 +38,9 @@ public class ListenerClass implements Listener {
        Set<Player> players = event.getMatch().getPlayers();
 
        RunnableClass runnableClass = new RunnableClass(plugin, players);
+        
        ongoingRunnables.put(arena, runnableClass);
-       ongoingRunnables.put(arena, task);
+       
 
 
 
