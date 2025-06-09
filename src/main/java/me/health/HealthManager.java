2f96c91639f933d07d7745e7b4a798607777b4b0
Ryan Tapfield <108175935+RyanDusty@users.noreply.github.com>
2025-06-10 09:07:25 +1000

    Update HealthManager.java

--- src/main/java/me/health/HealthManager.java
+++ src/main/java/me/health/HealthManager.java
@@ -1,1 +1,15 @@
+package me;
 
+import me.config.ConfigClass;
+import me.realized.duels.api.Duels;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+
+public class HealthManager {
+  private Plugin plugin;
+  public HealthManager(Plugin plugin) {
+    this.plugin = plugin;
+  }
+}
