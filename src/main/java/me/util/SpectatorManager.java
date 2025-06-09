4393f0deffbee3c4aa653bc379ffae680cdbf7cc
Ryan Tapfield <108175935+RyanDusty@users.noreply.github.com>
2025-06-10 09:16:53 +1000

    Update SpectatorManager.java

--- src/main/java/me/util/SpectatorManager.java
+++ src/main/java/me/util/SpectatorManager.java
@@ -1,1 +1,14 @@
+package me;
 
+public class SpectatorManager() {
+  private static CustomHealthDisplay plugin;
+  private Spectator spectator;
+
+  public SpectatorManager(CustomHealthDisplay plugin, Spectator spectator) {
+    this.plugin = plugin;
+    this.spectator = spectator;
+  }
+  public void setSpectators() {
+    this.spectator = spectator;
+  }
+}
