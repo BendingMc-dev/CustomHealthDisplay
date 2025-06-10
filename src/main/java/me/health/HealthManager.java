ff014dd87ceee635b2f82d816c457d048bfe7613
Ryan Tapfield <108175935+RyanDusty@users.noreply.github.com>
2025-06-11 09:17:10 +1000

    Update HealthManager.java

--- src/main/java/me/health/HealthManager.java
+++ src/main/java/me/health/HealthManager.java
@@ -9,9 +9,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class HealthManager {
   private static CustomHealthDisplay plugin;
-  
-  public HealthManager(CustomHealthDisplay plugin) {
+  private Set<Players> = MatchRunnable.getPlayers();
+  private Spectator spectator = SpectatorRunnable.getSpectators();
+  private String message = players + "test" + spectators
+  public HealthManager(CustomHealthDisplay plugin, Set<Players> players, Spectator spectator) {
+    this.players = players;
+    this.spectator = spectator;
     this.plugin = plugin;
   }
-  public actionBar
+  public static void sendActionBar(Set<Players> players, Spectator spectator, String message) {
+    CustomCooking actionbar = CustomCooking.playerActionBar();
+    
+  }
 }
